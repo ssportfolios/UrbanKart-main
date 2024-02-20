c074977b2a4046757a956918955387aecad09b44
sivavk18 <sivakishan77788@gmail.com>
2024-02-20 10:49:08 +0530

    first commit

---
+++ UrbanKart_Frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; // Import React
+
+import { Main, Product, Footer } from "../components";
+
+function Home() {
+  return (
+    <>
+      <Main />
+      <Product />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
